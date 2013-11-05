a8ec3ce52771f6488a5749b80d08c86a5771e14a
Petri Savolainen <petri.savolainen@iki.fi>
2013-11-05 13:13:02 +0200

    add missing transaction types

--- banking/statements/nordea/__init__.py
+++ banking/statements/nordea/__init__.py
@@ -1,1 +1,29 @@
+# Transaction type mapping for Nordea statement parsers
+
+TRANSACTION_TYPES = {
+   "TILISIIRTO": "XFER",
+   "PALVELUMAKSU": "FEE",
+   "PKORTTIMAKSU": "POS",
+   "VIITESIIRTO": "XFER",
+   "PIKASIIRTO": "XFER",
+   "ETUUS": "XFER",
+   "AUTOMAATTINOSTO": "ATM",
+   "MAKSUPALVELU": "REPEATPMT",
+   "TALLETUSKORKO": "INT",
+   "LÄHDEVERO": "FEE",
+   "SUORAVELOITUS": "DIRECTDEBIT",
+   "PANO": "DEP",
+   "VUOKRA": "PAYMENT",
+   "VERONPALAUTUS": "DIRECTDEP",
+   "PALKKA": "DIRECTDEP",
+   "VISA ELECTRON-OSTO": "DIRECTDEBIT", # Added for Nordea
+   "ATMOTTO/OTTO.": "ATM", # Added for Nordea
+   "PALVELUMAKSU ALV 0%": "FEE",
+   "ITSEPALVELU": "ATM",
+   "E-MAKSU": "PAYMENT",
+}
+
+#   Other types available: 'CREDIT', 'DEBIT', 'INT', 'DIV', 'DEP',
+#   'CHECK', 'CASH', 'OTHER'
+
 
